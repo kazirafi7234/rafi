e6464fff32c5fff64e9f6e98dca1bd5737887bf4
kazirafi7234 <168292988+kazirafi7234@users.noreply.github.com>
2024-04-30 16:26:19 +0000

    Container config done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+("use strict"); // treat all JS code as newer version
+
+
+// alert(3 +3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Rafi");
+
+let name = "Rafi"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => big powers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Rafi") // string
+console.log(typeof age) // number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
