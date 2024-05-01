2a29167bc52d5e170ccd4e37f9ec4c2ab1148713
kazirafi7234 <168292988+kazirafi7234@users.noreply.github.com>
2024-05-01 13:36:04 +0000

    Container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// score at "33abc" = typeof is string, valueInNumber is only number and it will show NaN
+// score at null = typeof is object, valueInNumber is only number and it will show 0
+// score at undefined = typeof is undefined, valueInNumber is only number and it will show NaN
+// score at true = typeof is boolean, valueInNumber is only number and it will show 1
+// score at false = typeof is boolean, valueInNumber is only number and it will show 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// If blank/ 0 => false
+// "Rafi" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
