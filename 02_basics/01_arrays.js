4960f55fdde755d38166e0cd07f50f2ca2821c7c
kazirafi7234 <168292988+kazirafi7234@users.noreply.github.com>
2024-05-18 17:01:49 +0000

    new series

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+
+console.log(myArr[0])
+
+const myHeroes = ["Iron man", "Hulk"]
+const myArr2 = new Array(1, 2, 3, 4)
+
+//  methods 
+
+myArr.push(6)
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
+
+myArr.unshift(0)
+console.log(myArr)
+
+myArr.shift()
+console.log(myArr)
+
+console.log(myArr.includes(7))
+console.log(myArr.indexOf(3))
+console.log(myArr.indexOf(11))
+
+const newArr = myArr.join()
+console.log(myArr)
+console.log(newArr)
+console.log(typeof newArr)
+
+// slice, splice
+
+console.log("A ", myArr)
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1)
+console.log("B ", myArr)
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2)
+console.log("C ", myArr)
+
+
