8b95de212de8236465b50f14ed0de40df0448dc4
kazirafi7234 <168292988+kazirafi7234@users.noreply.github.com>
2024-08-12 15:08:23 +0000

    objects complete

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,19 @@
+const course = {
+    coursename: "JS is hindi",
+    price: "000",
+    courseInstructor: "rafi"
+}
+
+const {courseInstructor} = course
+console.log(courseInstructor)
+
+
+// api
+
+{
+    "name"; "rafi",
+    "coursename"; "js in hindi",
+    "price"; "999"
+}
+
+
