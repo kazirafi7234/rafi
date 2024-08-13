eba78cad2b84ea26ceeb406ef75760c90aac1305
kazirafi7234 <168292988+kazirafi7234@users.noreply.github.com>
2024-08-13 13:46:12 +0000

    .

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,4 +29,31 @@ function loginUserMessage(username){
 console.log(loginUserMessage("Rafi"))
 
 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 600))
+
+
+const user = {
+    username: "rafi",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
+
 
