8873c6f20f9cc276eef3d88cc3d92bb845ad64d7
kazirafi7234 <168292988+kazirafi7234@users.noreply.github.com>
2024-08-18 18:59:54 +0000

    .

---
+++ 05_iterations/four.js
@@ -0,0 +1,25 @@
+const myObject = {
+    js: "javascipt",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('Fr', "France")
+
+//for (const key in map) {
+//    console.log(key)}
+
+
